775ea086833a254ce1ba2b3e6b36bb01a8610267
franciscobeltranestrada <82306054+franciscobeltranestrada@users.noreply.github.com>
2021-06-11 12:36:33 -0400

    Add files via upload

---
+++ MiPrimerPrograma.java/src/module-info.java
@@ -0,0 +1,2 @@
+module MiPrimerPrograma.java {
+}
---
+++ MiPrimerPrograma.java/src/p1/Programa.java
@@ -0,0 +1,8 @@
+package p1;
+public class Programa{
+	public static void main(String[]args) {
+		System.out.println("Mi nombre es Coding Dojo\r\n"
+				+ "Tengo 100 años de edad.\r\n"
+				+ "Mi ciudad es Burbank, CA");
+	}
+}
